e047e34caa7bee9a48b8c23173807073d3bde8c3
sdarends <s.d.arends@hotmail.com>
2020-11-23 15:37:25 +0100

    added default file and updated requirements

---
+++ default_test.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+
+driver = webdriver.Chrome()
+driver.get("https://www.google.com/")
+driver.set_window_size(668, 680)
+driver.find_element(By.NAME, "q").send_keys("restaurant near me")
+driver.find_element(By.NAME, "q").send_keys(Keys.ENTER)
+time.sleep(10)
