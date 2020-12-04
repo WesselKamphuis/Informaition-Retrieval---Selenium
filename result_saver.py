6f4c6607a0a2a3a5c8a88b1e1a678a840aeff92f
sdarends <s.d.arends@hotmail.com>
2020-12-04 09:47:11 +0100

    added translate.google.com as spam

--- result_saver.py
+++ result_saver.py
@@ -3,8 +3,9 @@ def result_saver(driver, folder_path, name):
     links = driver.find_elements_by_css_selector(".rc a")
     spam1 = 'https://webcache.googleusercontent.com'
     spam2 = 'https://www.google.com'
+    spam3 = 'https://translate.google.com'
     for link in links:
         a = link.get_attribute('href')
-        if (spam1 not in a) and (spam2 not in a):
+        if (spam1 not in a) and (spam2 not in a) and (spam3 not in a):
             f.write(a + '\n')
     f.close()
