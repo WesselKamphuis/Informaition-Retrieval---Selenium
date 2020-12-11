039212d518c441e5fbb30e728e920dd4b2fbf396
sdarends <s.d.arends@hotmail.com>
2020-12-11 10:01:25 +0100

    Changed the folders and queries based upon the most relevant searchterms, also changed 'partijbeleid' to 'standpunten'

--- main.py
+++ main.py
@@ -4,19 +4,22 @@ from Experiment import browser
 def main():
     dict_of_queries = {
         'Restaurant_Near_Me': 'restaurant near me',
-        'Belastingen': 'belastingen',
-        'Defensie': 'defensie',
+        'Pensioenen': 'pensioenen',
+        'Veiligheid': 'veiligheid',
         'Europese_Unie': 'europese unie',
         'Klimaat': 'klimaat',
         'Onderwijs': 'onderwijs',
-        'Vluchtelingen': 'vluchtelingen',
+        'Immigratie': 'immigratie',
+        'Werkgelegenheid': 'werkgelegenheid',
+        'Corona_aanpak': 'corona aanpak',
+        'Woningbouw': 'woningbouw',
         'Zorg': 'zorg'}
 
     for key in dict_of_queries.keys():
         if key == 'Restaurant_Near_Me':
             query = 'restaurant near me'
         else:
-            query = 'partijbeleid ' + dict_of_queries.get(key)
+            query = 'standpunten ' + dict_of_queries.get(key)
         browser.browser(query, key)
 
 
