3a7ce2c47bbda70b7d0120c6e058b0971cbecbfb
sdarends <s.d.arends@hotmail.com>
2020-12-15 17:27:02 +0100

    added the method for calculating the jaccard index from https://www.statology.org/jaccard-similarity-python/. Saved the results in a text file.

--- validator.py
+++ validator.py
@@ -1,14 +1,20 @@
 # checks if two files are identical
 import os
 import scipy.stats as stats
+import pandas as pd
 
 
+def jaccard(file1, file2):
+    intersection = len(list(set(file1).intersection(file2)))
+    union = (len(file1) + len(file2)) - intersection
+    return float(intersection) / union
 def main():
     results = os.listdir('Preprocessed_results')
 
     for sub_folder in results:
         theme = 'Preprocessed_results/' + sub_folder
         print('\n', sub_folder)
+        pd.DataFrame
         for file in os.listdir(theme):
             if file != '[Party - Municipality]':
                 f1_path = theme + '/Groenlinks - Nijmegen.txt'
@@ -23,7 +29,8 @@ def main():
                     l1.append(line1)
                 for line2 in f2:
                     l2.append(line2)
-                print(file, ': ', stats.kendalltau(l1, l2)[0])
+                # print(file, ': ', stats.kendalltau(l1, l2)[0]
+                print(file, ': ', jaccard(l1, l2))
 
 
 if __name__ == '__main__':
